a72c0ba6dbce960b4853c2b3bd0897568b99fc4e
babu <karanamseshubabu28@gmail.com>
2024-07-27 07:33:13 +0530

    added

--- tests/Annations.spec.js
+++ tests/Annations.spec.js
@@ -18,3 +18,15 @@ test.skip('test3',async({page})=>{
 test.fixme('test4',async({page})=>{
     console.log('i am from test4')
 })
+
+test('test5',async({page})=>{
+    expect(1).toBe(1)
+    test.fail()
+    console.log('i am from test5')
+})
+
+test('test6',async({page})=>{
+  //  test.slow()
+  test.setTimeout(2000)
+    console.log('i am from test5')
+})
