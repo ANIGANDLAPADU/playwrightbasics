7eba3b1db31b9480bc5f69a64ab1383b89ec1408
babu <karanamseshubabu28@gmail.com>
2024-07-29 07:22:54 +0530

    allure reports

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,13 +1,17 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
+let page
+test.beforeEach(async({browser})=>{
+   page= await browser.newPage()
+   page.goto("https://www.youtube.com/")
+  const button= await page.locator("")
+   await button.click({button:'right'})
+   page.on('dialog',async dialog=>{
 
-test('has title', async ({ page }) => {
-  await page.goto("https://testautomationpractice.blogspot.com/")
-   page.on('dialog',async(dialog)=>{
-    expect(dialog.type()).toContain('alert')
-    expect(dialog.message()).toContain('I am an alert box!')
-   await dialog.accept()
+   }){
+     
    })
-   await page.click("//*[@id='HTML9']/div[1]/button[1]")
-   await page.waitForTimeout(5000)
-});
+})
+test.afterEach(async()=>{
+    
+})
