b3909ed8b989b27f70234cbb43e7496ae98f87d3
babu <karanamseshubabu28@gmail.com>
2024-07-29 17:48:14 +0530

    updated

--- tests/AutoSuggestDropDown.spec.js
+++ tests/AutoSuggestDropDown.spec.js
@@ -1,5 +1,13 @@
 import {test,expect} from '@playwright/test'
 test('AutoSuggest', async ({page})=>{
-    await page.goto('https://www.redbus.in/')
-    await page.locator("//input[@id='src']").click
+ 
+    await page.goto('https://www.google.co.in/')
+     page.fill("//textarea[@class='gLFyf']",'seshu')
+     const drops= await page.$$('//div[@class="wM6W7d"]/span')
+     for(const drop of drops){
+        test.slow()
+       const data= await drop.innerText
+       console.log(data)
+     }
+     await page.waitForTimeout(5000)
 })
